28a31116d23a48267ebdab0eb82b45bc6d3ed564
jarrodl <jarrod@luciatech.net>
2018-02-21 09:47:45 +1100

    update start script

--- start.sh
+++ start.sh
@@ -220,8 +220,4 @@ fi
 touch $OUTPUT_LOGFILES
 tail -f $OUTPUT_LOGFILES &
 
-curl -XPUT 'localhost:9200/_template/afm?pretty' -H 'Content-Type: application/json' -d @/etc/elasticsearch/scripts/afm_mapping.json
-curl -XPUT 'localhost:9200/_template/dns?pretty' -H 'Content-Type: application/json' -d @/etc/elasticsearch/scripts/dns_mapping.json
-curl -XPUT 'localhost:9200/_template/pem?pretty' -H 'Content-Type: application/json' -d @/etc/elasticsearch/scripts/pem_mapping.json
-
 wait
